8dc9cccb89038e527d4f0e05333637d9495fe819
techhadera <rdahaderv@gmail.com>
2021-09-28 00:59:04 +0300

    fix: database name

--- hadoop/src/2.sql
+++ hadoop/src/2.sql
@@ -1,6 +1,6 @@
 select nn.name, nn.year_, sn.count_
-from znu_names.national_names nn
-join znu_names.state_names sn
-  on (sn.name = nn.name and sn.year_ = nn.year_)
+from znu_on_delete.national_names nn
+join znu_on_delete.state_names sn
+on (sn.name = nn.name and sn.year_ = nn.year_)
 order by nn.year_ desc
 limit 10;
